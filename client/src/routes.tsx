6e345200d1f2b94bbcf34df95a797042866c36ff
qister <qistersick@gmail.com>
2021-01-12 22:03:05 +0300

    убрано лишнее

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -3,14 +3,14 @@ import { Switch, Route, Redirect, Router } from 'react-router-dom'
 
 import { Authorization } from './pages/Authorization'
 import { AddMeme } from './components/AddMeme'
-import { Main_ } from './components/Main'
+import { Main } from './components/Main'
 
 export const useRoutes = (isAuthenticated: boolean) => {
   if (isAuthenticated) {
     return (
       <Switch>
         <Route path='/' exact>
-          <Main_ />
+          <Main />
         </Route>
         <Route path='/add' exact>
           <AddMeme />
