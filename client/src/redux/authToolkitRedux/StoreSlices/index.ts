d5e7a3e4ec46f1088faa0d9399d3645cb27ad3dd
qister <qistersick@gmail.com>
2021-02-17 00:18:53 +0300

    Добавлена пагинация на фронте и бэке

--- client/src/redux/authToolkitRedux/StoreSlices/index.ts
+++ client/src/redux/authToolkitRedux/StoreSlices/index.ts
@@ -1,11 +1,15 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from 'redux'
 
-import authorization from './authorization';
-import app from './app';
+import authorization from './authorization'
+import app from './app'
 
+import { store } from 'components/App/AppBehavior'
 
 export const reducers = combineReducers({
-    authorization, app
-});
+  authorization,
+  app,
+})
 
-export type RootState = ReturnType<typeof reducers>;
+// export type RootState = ReturnType<typeof reducers>
+
+export type RootState = ReturnType<typeof store.getState>
