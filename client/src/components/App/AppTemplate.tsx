037ded50fbd6cc6c942c5f1feab8ee8470c08a89
qister <qistersick@gmail.com>
2021-03-02 23:51:30 +0300

    авторизация и регистрация переписаны на antd

--- client/src/components/App/AppTemplate.tsx
+++ client/src/components/App/AppTemplate.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { Route, Switch, Redirect, BrowserRouter as Router  } from 'react-router-dom'
+import {
+  Route,
+  Switch,
+  Redirect,
+  BrowserRouter as Router,
+} from 'react-router-dom'
 
 import { RootState } from '../../redux/authToolkitRedux/StoreSlices'
 import { AppLayout } from 'components/AppLayout'
 import { Authorization } from '../../pages/Authorization'
 import '../../styles/App.scss'
 import 'materialize-css'
+import { Registration } from 'pages/Registration/Registration'
+import { LoginForm } from 'pages/Authorization/Login'
 
 export function AppTemplate() {
   const isAuthenticated = useSelector(
@@ -14,22 +21,23 @@ export function AppTemplate() {
   )
   // const isAuthenticated = true
 
-  const routes = isAuthenticated ?
+  const routes = isAuthenticated ? (
     <>
-      <Route path='/' component={AppLayout} /> 
+      <Route path='/' component={AppLayout} />
       <Redirect to='/feed' />
     </>
-  : 
+  ) : (
     <>
-      <Route path='/login' component={Authorization} />
-      <Redirect to='/login' />
+      {/* <Route path='/login' component={Authorization} /> */}
+      <Route path='/login' component={LoginForm} exact/>
+      <Route path='/register' component={Registration} exact/>
+      {/* <Redirect to='/login' /> */}
     </>
+  )
 
   return (
     <Router>
-      <Switch>
-        {routes}
-      </Switch>
+      <Switch>{routes}</Switch>
     </Router>
   )
 }
--- client/src/pages/Authorization/AuthorizationTemplate.tsx
+++ client/src/pages/Authorization/AuthorizationTemplate.tsx
@@ -1,66 +1,78 @@
-import React from "react";
-import { Form, Input, Button, Checkbox } from "antd";
+import React from 'react'
+import { Form, Input, Button, Checkbox } from 'antd'
 
 import { AuthForm } from '../../constants/types'
-import "./Authorization.scss";
+import './Authorization.scss'
+import { LockOutlined, UserOutlined } from '@ant-design/icons'
 
 interface Props {
-  form: AuthForm,
-  changeHandler(event: React.ChangeEvent<HTMLInputElement>): void;
+  form: AuthForm
+  changeHandler(event: React.ChangeEvent<HTMLInputElement>): void
   loginHandler(
-    event: React.MouseEvent<HTMLElement> | React.FormEvent<HTMLFormElement>
-  ): void;
+    event: React.MouseEvent<HTMLElement> | React.FormEvent<HTMLFormElement>,
+  ): void
 }
 
 const layout = {
   labelCol: { span: 8 },
   wrapperCol: { span: 16 },
-};
+}
 const tailLayout = {
   wrapperCol: { offset: 8, span: 16 },
-};
+}
 
 export function AuthorizationTemplate({
   form,
   changeHandler,
   loginHandler,
 }: Props): JSX.Element {
-
   const { email, password } = form
 
-  console.log('AuthorizationTemplate email', email);
-  console.log('AuthorizationTemplate password', password);
+  console.log('AuthorizationTemplate email', email)
+  console.log('AuthorizationTemplate password', password)
 
-  const ROOT_CLASS = "authorization";
+  const ROOT_CLASS = 'authorization'
   return (
     <div className={ROOT_CLASS}>
-      <Form
-        {...layout}
-        name="basic"
-        initialValues={{ remember: true }}
-      >
-        <Form.Item
-          label="Email"
-          name='email'
-          getValueFromEvent={changeHandler}
-        >
-          <Input name='email' defaultValue={email} />
+      <Form {...layout} name='basic' initialValues={{ remember: true }}>
+        <Form.Item name='email' getValueFromEvent={changeHandler}>
+          {/* <Input name='email' defaultValue={email} /> */}
+          <Input
+            name='email'
+            prefix={<UserOutlined className='site-form-item-icon' />}
+            placeholder='Username'
+          />
         </Form.Item>
 
-        <Form.Item
-          label="Password"
-          name='password'
-          getValueFromEvent={changeHandler}
-        >
-          <Input.Password name='password' defaultValue={password} />
+        <Form.Item name='password' getValueFromEvent={changeHandler}>
+          {/* <Input.Password name='password' defaultValue={password} /> */}
+          <Input
+            prefix={<LockOutlined className='site-form-item-icon' />}
+            type='password'
+            placeholder='Password'
+            name='password'
+            defaultValue={password}
+          />
         </Form.Item>
 
-        <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+        <Form.Item {...tailLayout} name='remember' valuePropName='checked'>
           <Checkbox>Remember me</Checkbox>
         </Form.Item>
 
         <Form.Item {...tailLayout}>
-          <Button type="primary" onClick={loginHandler}>Log in</Button>
+          <Form.Item name='remember' valuePropName='checked' noStyle>
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <a className='login-form-forgot' href=''>
+            Forgot password
+          </a>
+        </Form.Item>
+
+        <Form.Item {...tailLayout}>
+          <Button type='primary' onClick={loginHandler}>
+            Log in
+          </Button>
         </Form.Item>
       </Form>
 
@@ -72,5 +84,5 @@ export function AuthorizationTemplate({
               Регистрация
             </button> */}
     </div>
-  );
+  )
 }
