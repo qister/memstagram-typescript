8a1169f2fb8d6cd2e6c40bfeb04cc9369e9fe91a
Alex Lyakhnitskiy <62909771+qister@users.noreply.github.com>
2022-04-26 20:28:29 +0300

    chore: remove redux (#109)

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,10 @@
 import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from 'react-query'
 
 import 'antd/dist/antd.min.css'
 
 import { App } from './components/App/App'
-import { createStore } from 'redux/store'
 import { ErrorBoundary } from 'utils/ErrorBoundary'
 import { AuthProvider } from 'auth'
 
@@ -14,15 +12,13 @@ const queryClient = new QueryClient()
 
 ReactDOM.render(
   <ErrorBoundary>
-    <Provider store={createStore()}>
-      <QueryClientProvider client={queryClient}>
-        <AuthProvider>
-          <Router>
-            <App />
-          </Router>
-        </AuthProvider>
-      </QueryClientProvider>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <Router>
+          <App />
+        </Router>
+      </AuthProvider>
+    </QueryClientProvider>
   </ErrorBoundary>,
   document.getElementById('root'),
 )
