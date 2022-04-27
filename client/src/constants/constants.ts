db2b8bfca33a43d6b7c77a8ad3e873961e5fc34e
Alexander Lyakhnitskiy <qistersick@gmail.com>
2022-04-28 01:54:21 +0300

    constants back

---
+++ client/src/constants/constants.ts
@@ -0,0 +1,22 @@
+import { ContentPath } from './enums'
+
+const { Feed, Statistics, Profile, Add } = ContentPath
+
+export const MENU_SIDEBAR_ITEMS = [
+  {
+    key: Feed,
+    title: 'Лента',
+  },
+  {
+    key: Statistics,
+    title: 'Статистика',
+  },
+  {
+    key: Profile,
+    title: 'Профиль',
+  },
+  {
+    key: Add,
+    title: 'Добавить',
+  },
+]
