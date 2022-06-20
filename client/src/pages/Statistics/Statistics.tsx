a084effa8ac7fdaaefc69079fb82a80e6839f820
Alex Lyakhnitskiy <62909771+qister@users.noreply.github.com>
2022-06-21 01:05:38 +0300

    feat: base user statistics added (#121)

--- client/src/pages/Statistics/Statistics.tsx
+++ client/src/pages/Statistics/Statistics.tsx
@@ -1,3 +1,26 @@
+import { Row, Col, Statistic } from 'antd'
+
+import { useUserStatistics } from 'API/userApi'
+
 export const Statistics = () => {
-  return <div>Статистика</div>
+  const { data, isLoading } = useUserStatistics()
+
+  return (
+    <Row gutter={16}>
+      <Col span={12}>
+        <Statistic
+          title="Поставлено лайков"
+          value={data?.data.likedMemesCount}
+          loading={isLoading}
+        />
+      </Col>
+      <Col span={12}>
+        <Statistic
+          title="Загружено мемов"
+          value={data?.data.uploadedMemesCount}
+          loading={isLoading}
+        />
+      </Col>
+    </Row>
+  )
 }
